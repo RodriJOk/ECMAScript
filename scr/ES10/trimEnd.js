7eeaaa8aa06360f34ae12052ffc8abe4a18b10f8
Rodrigo Juarez <juarezrodrigo59@gmail.com>
2021-05-02 18:56:04 -0300

    ES10 - trimEnd()

---
+++ scr/ES10/trimEnd.js
@@ -0,0 +1,7 @@
+//Otra de las nuevas incorporaciones de ES10 fue la inclusion de trimEnd()
+//Este metodo me permite quitar los espaciados al final de un string
+//Veamos un ejemplo: 
+
+let hello = "Hello World         "
+console.log(hello.trimEnd())
+//Hello World
