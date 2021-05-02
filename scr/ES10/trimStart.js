50a52309f472518e1c066372bcb652076611f1a6
Rodrigo Juarez <juarezrodrigo59@gmail.com>
2021-05-02 18:53:02 -0300

    ES10 - trimStart()

---
+++ scr/ES10/trimStart.js
@@ -0,0 +1,7 @@
+//Este es otro metodo que fue agregado a ES10. 
+//El metodo, me permite quitar los espaciados en blanco antes de un string. 
+//Veamos un ejemplo: 
+
+let hello = "         Hello World"
+console.log(hello.trimStart())
+//Hello World
