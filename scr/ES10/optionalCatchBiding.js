cd9e68d535e01a91aad032cc6eb377795e0f610f
Rodrigo Juarez <juarezrodrigo59@gmail.com>
2021-05-02 19:00:42 -0300

    ES10 - OpcionalCatchBiding

---
+++ scr/ES10/optionalCatchBiding.js
@@ -0,0 +1,8 @@
+//Esta caracteristica ya nos permite elegir entre pasar el parametro de error en el catch, try
+//Veamos un ejemplo. 
+
+try {
+    
+} catch /*(error) ya no es necesario colocarlo*/ {
+    error
+}
