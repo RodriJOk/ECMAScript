b31447ba9b5704bb7b1fac5e976161226e21d5a9
Rodrigo Juarez <juarezrodrigo59@gmail.com>
2021-05-02 13:45:39 -0300

    Object Values

--- scr/ES8/Object-values.js
+++ scr/ES8/Object-values.js
@@ -1,1 +1,15 @@
 //Tambien en la incorporacion en ES8 
+//Object Values devuelve los valores de un objeto a un arreglo. 
+//Veamos un ejemplo para entender este metodo.
+
+const data = {
+    frontend: "Rodrigo",
+    backend: "Jonathan",
+    devops: "David",
+    tester: "Woody",
+}
+
+const transformacion = Object.values(data)
+console.log(transformacion)
+//[ 'Rodrigo', 'Jonathan', 'David', 'Woody' ]
+console.log(transformacion.length) //4
