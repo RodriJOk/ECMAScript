8142fdacd5ec0cacf08a70a374ac6f95927567dc
Rodrigo Juarez <juarezrodrigo59@gmail.com>
2021-05-19 19:00:00 -0300

    Add modifications in allSettled

---
+++ scr/ES11/promise-AllSettled.js
@@ -0,0 +1,20 @@
+//Una de las incorporacion en ES11 es la incoporacion de la propiedad AllSettled.
+//El metodo Promise.allSettled recibe un array de promesas y se resuelve cuando estas hayan sido concluidas,
+//sin importar si fueron resueltas(resolve) o rechazadas(reject)
+//Veamos un ejemplo para entenderlo mejor. 
+
+const p1 = new Promise((resolve, reject)=> resolve("Promesa resuelta"));
+const p2 = new Promise((resolve, reject)=> reject("Promesa rechazada"));
+const p3 = new Promise((resolve, reject)=> resolve("Promesa resuelta"));
+
+Promise.allSettled([p1, p2, p3])
+    .then(response => console.log(response))
+    .catch(error => console.log(error))
+
+/*
+[
+    {status: "fulfilled", value: "Promesa resuelta"},
+    {status: "rejected", reason: "Promesa rechazada"},
+    {status: "fulfilled", value: "Promesa resuelta"}
+]
+*/    
