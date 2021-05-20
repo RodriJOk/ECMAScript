97fae69286e3a1a9dfc3e92414621d421fbd1e6d
Rodrigo Juarez <juarezrodrigo59@gmail.com>
2021-05-20 15:21:27 -0300

    Add numberLiterals.js

---
+++ scr/ES12/numberLiterals.js
@@ -0,0 +1,17 @@
+//Otra de las nuevas caracteristicas de ES12 es la incorporacion de los Literales Numericos. 
+//Esta es una nueva funcionalidad que nos permite separar VISUALMENTE grande cantidades de datos. 
+
+//Veamos un ejemplo para entenderlo mejor: 
+//Imaginemos que tenemos un numero muy grande.
+//Y queremos separalo visualmente
+console.log(123_313_212_123_121) //123313212123121
+
+//En este ejemplo al utilizar el _ (guion bajo) Javascript va poder respresentar ese dato.
+
+//Esto tambien aplica a binarios 
+
+console.log(0b1111_011101001_1000100100); // 8103460
+
+//Y tambien con exadecimales
+
+console.log(0xFAB_F00D); //26296349
